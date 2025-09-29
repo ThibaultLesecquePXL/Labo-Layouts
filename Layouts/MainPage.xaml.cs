3d80c12150f0d5082b4f2562afa08e14eada1b52
ThibaultLesecquePXL <thibault.lesecque@student.pxl.be>
2025-09-29 14:29:19 +0200

    Deel 2

--- Layouts/MainPage.xaml.cs
+++ Layouts/MainPage.xaml.cs
@@ -2,11 +2,14 @@
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
-
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        public async void GridPageClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync(nameof(GridPage));
         }
 
     }
